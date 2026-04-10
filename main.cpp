27e3ca8b4c9c3c9196f4193ff61013f75a722dab
Student <mcsemen55577719@gmail.com>
2026-04-10 18:42:20 +0700

    delete directory build

--- main.cpp
+++ main.cpp
@@ -8,7 +8,17 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+
     const std::string filename = "/home/student/parcer/Transport-noise/st1_ac.00";
-    parseFile(filename);
+
+    try {
+        parseFile(filename);
+        return 0; // Успешное завершение
+    } catch (const std::exception &ex) {
+        // Выводим сообщение об ошибке в стандартный поток ошибок (stderr)
+        std::cerr << "[ОШИБКА] " << ex.what() << '\n';
+        return 1; // Код ошибки при завершении программы
+    }
+
     return a.exec();
 }
